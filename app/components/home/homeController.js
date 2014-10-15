147c9229a60e2d6e334ebb80da140166b7bdeb0d
Emilio Borges <emilio.j.borges@gmail.com>
2014-10-15 01:45:50 -0400

    Got routes, views, and controllers working
    ...with special emoticon animations!

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -9,9 +9,24 @@ module.controller(
     [
         '$scope',
         '$location',
-        function($scope, $location){
+        '$interval',
+        function($scope, $location, $interval){
+
+            var frames = ['┗ (･o･ ) ┓', '┏(･o･)┛', '┗ ( ･o･) ┓', '┏(･o･)┛' ], // 4 frames
+                framesCount = frames.length,
+                frameIndex = 0;
+
+            $interval(function() {
+                frameIndex = (frameIndex + 1) % framesCount;
+                $scope.emoteFrame = frames[frameIndex];
+            }, 250);
+
+            $scope.$on('$destroy', function() {
+                $interval.cancel();
+            });
 
             $scope.gotoProjects = function() {
+                $interval.cancel();
                 $location.path('/projects').replace();
             };
 
--- app/components/projects/projectsController.js
+++ app/components/projects/projectsController.js
@@ -9,9 +9,24 @@ module.controller(
     [
         '$scope',
         '$location',
-        function($scope, $location){
+        '$interval',
+        function($scope, $location, $interval){
+
+            var frames = ["< ('o'<)", "( '-’ )", "(>‘o’)>", "v( ‘.’ )v", "< (' .' )>", "< ('.'<)", "( '.’ )", "(>‘.’)>", "v( ‘.’ )v", "< (' .' )>"], // 10 frames
+                framesCount = frames.length,
+                frameIndex = 0;
+
+            $interval(function() {
+                frameIndex = (frameIndex + 1) % framesCount;
+                $scope.emoteFrame = frames[frameIndex];
+            }, 250);
+
+            $scope.$on('$destroy', function() {
+                $interval.cancel();
+            });
 
             $scope.gotoHome = function() {
+                $interval.cancel();
                 $location.path('/home').replace();
             };
 
